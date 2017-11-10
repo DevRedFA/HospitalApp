76171e98eea49e539646133d91357611f1160cf7
Mikhail Chtetsov <chtetsoff@gmail.com>
2017-11-10 23:19:26 +0300

    RoleDaoImpl

--- src/main/java/com/epam/hospital/TestMain.java
+++ src/main/java/com/epam/hospital/TestMain.java
@@ -1,9 +1,6 @@
 package com.epam.hospital;
 
-import com.epam.hospital.dao.api.AppointmentDao;
-import com.epam.hospital.dao.api.PatientAppointmentDao;
-import com.epam.hospital.dao.api.PatientDiagnosisDao;
-import com.epam.hospital.dao.api.UserDao;
+import com.epam.hospital.dao.api.*;
 import com.epam.hospital.dao.impl.*;
 import com.epam.hospital.model.*;
 import com.epam.hospital.util.HibernateUtil;
@@ -14,20 +11,27 @@ import java.sql.Timestamp;
 public class TestMain {
     public static void main(String[] args) {
 
-        UserDao userDao = new UserDaoImpl();
-        User user = userDao.getUserById(2);
-        PatientAppointment patientAppointment = new PatientAppointment();
-        Patient patient = new PatientDaoImpl().getPatientById(1);
-        patientAppointment.setAppointedBy(user);
-        patientAppointment.setPatient(patient);
-        AppointmentDao appointmentDao = new AppointmentDaoImpl();
-        Appointment appointmentById = appointmentDao.getAppointmentById(1);
-        System.out.println(appointmentById.getName());
-        patientAppointment.setAppointment(appointmentById);
-        patientAppointment.setAppointedDate(new Timestamp(System.currentTimeMillis()));
+//        UserDao userDao = new UserDaoImpl();
+//        User user = userDao.getUserById(2);
+//        PatientAppointment patientAppointment = new PatientAppointment();
+//        Patient patient = new PatientDaoImpl().getPatientById(1);
+//        patientAppointment.setAppointedBy(user);
+//        patientAppointment.setPatient(patient);
+//        AppointmentDao appointmentDao = new AppointmentDaoImpl();
+//        Appointment appointmentById = appointmentDao.getAppointmentById(1);
+//        System.out.println(appointmentById.getName());
+//        patientAppointment.setAppointment(appointmentById);
+//        patientAppointment.setAppointedDate(new Timestamp(System.currentTimeMillis()));
+//
+//        PatientAppointmentDao patientAppointmentDao = new PatientAppointmentDaoImpl();
+//        patientAppointmentDao.saveOrUpdatePatientAppointment(patientAppointment);
+        RoleDao roleDao = new RoleDaoImpl();
+        Role roleById = roleDao.getRoleById(1);
+        System.out.println("Role by id = " + roleById.getName());
 
-        PatientAppointmentDao patientAppointmentDao = new PatientAppointmentDaoImpl();
-        patientAppointmentDao.saveOrUpdatePatientAppointment(patientAppointment);
+        Role role_patient = roleDao.getRoleByName("ROLE_PATIENT");
+
+        System.out.println("Role by name = " + role_patient.getName() + " " + role_patient.getId());
 
 
         HibernateUtil.getSessionFactory().close();
