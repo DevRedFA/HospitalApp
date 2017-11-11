acf14187db4c3b3dda1f24e301b03729b4cdcc95
Sergey Pavlov <spb.pavlov@gmail.com>
2017-11-11 15:55:47 +0300

    DiagnosisGenerationTypeFix

--- src/main/java/com/epam/hospital/model/Diagnosis.java
+++ src/main/java/com/epam/hospital/model/Diagnosis.java
@@ -13,7 +13,7 @@ import java.util.Set;
 public class Diagnosis {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id",
             nullable = false)
     private Integer id;
